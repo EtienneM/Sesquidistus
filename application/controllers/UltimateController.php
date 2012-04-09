940f45eb261b7a0e172aaed61e1b5992dd2eb1b4
EtienneM <titizebioutifoul@free.fr>
2012-04-09 03:27:50 +0200

    La page Ultimate n'attends plus que la BDD Tout les templates sont mis en place. Il ne reste plus qu'une ou deux requête SQL à faire et c'est fonctionnel!

--- application/controllers/UltimateController.php
+++ application/controllers/UltimateController.php
@@ -6,10 +6,31 @@ class UltimateController extends Zend_Controller_Action
     public function init() {
         $this->view->headTitle()->append('L\'utlimate');
         $this->view->headLink()->appendStylesheet('/css/club/club.css');
-        $this->view->sections = array('toto', 'tmp');
+
+        $this->view->sections = array(
+            array(
+                'id' => 1,
+                'label' => 'Histoire de Frisbee',
+            ),
+            array(
+                'id' => 2,
+                'label' => 'Histoire d\'Ultimate',
+            ),
+        );
     }
 
     public function indexAction() {
+        $request = $this->getRequest();
+        $id = $request->getParam('id');
+        if (!isset($id)) {
+            $id = 1;
+        }
+        // Requête SQL pour trouver en fonction de l'id le contenu
+        $this->view->ultimate = array(
+            'titre' => 'biniou',
+            'contenu' => 'bliblablou'
+        );
+        $this->view->id = $id;
     }
 }
 
