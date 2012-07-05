0b343cb6e71e350812d505a4d18321e3c09a7699
EtienneM <titizebioutifoul@free.fr>
2012-07-05 20:56:56 +0200

    Modification du contact (#13)

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -26,9 +26,36 @@ class IndexController extends Zend_Controller_Action {
     }
 
     public function contactAction() {
+        $this->view->headScript()->appendFile('/js/jquery/jquery.validate.min.js')
+                ->appendFile('/js/jquery/jquery.validate.localization/messages_fr.js')
+                ->appendFile('/js/contact.js');
+        $this->view->headLink()->appendStylesheet('/css/contact.css');
         $contactMapper = new Application_Model_ContactMapper();
         $contacts = $contactMapper->fetchAll();
         $this->view->contact = $contacts[0];
+        $request = $this->getRequest();
+        $modifier = $request->getParam('modifier', false);
+        $this->view->modifier = $modifier;
+        $auth = Zend_Auth::getInstance();
+        if ($modifier && $auth->hasIdentity()
+                && $auth->getIdentity()->getRoleId() == Application_Model_Acl::ROLE_ADMIN) {
+
+            if (!is_null($firstName = $request->getParam('firstName')) && !is_null($email = $request->getParam('email'))) {
+                $contact = new Application_Model_Contact(array(
+                            'id' => 1,
+                            'prenom' => $firstName,
+                            'nom' => $request->getParam('lastName'),
+                            'telephone' => $request->getParam('phone'),
+                            'email' => $email,
+                        ));
+                $contactMapper->save($contact);
+                //$this->getRequest()->clearParams();
+                $this->_helper->flashMessenger('Modification du contact réussi');
+                $this->_redirect('/index/contact');
+            } else {
+                //TODO Message erreur
+            }
+        }
     }
 
     public function aproposAction() {
