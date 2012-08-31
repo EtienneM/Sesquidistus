fca55fa5bcd114e9c3b7a6243b1c5601d1cde3db
EtienneM <titizebioutifoul@free.fr>
2012-08-31 17:06:26 +0200

    Mise en forme pour le script...

--- scripts/mise-en-prod.sh
+++ scripts/mise-en-prod.sh
@@ -13,10 +13,9 @@ rm preprod/data/cache/*
 #--------- Backup ---------
 rm -R prod_bak
 cp -R prod prod_bak
+echo "Le backup prod_bak a été créé."
 
 echo "========== Synchronisation ========"
 rsync -rltDzn --stats preprod/ prod
 echo "========== Fin synchronisation ========"
-echo "Le backup prod_bak a été créé."
 cp index_prod.php prod/public/index.php
-
