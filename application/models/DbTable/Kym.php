0411214c18594394ae2ea55e25463f9c6bc2ae5b
Etienne Michon <etienne.michon@gmail.com>
2012-12-30 11:36:04 +0100

    Model for the Kym tab

---
+++ application/models/DbTable/Kym.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_Model_DbTable_Kym extends Zend_Db_Table_Abstract {
+    protected $_name = 'kym';
+    protected $_primary = 'id';   
+    protected $_sequence = true;  
+
+}
+
