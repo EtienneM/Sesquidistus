d1ef73bd246ae78fdd8a44c6ee02ce0b3160ae50
EtienneM <titizebioutifoul@free.fr>
2013-08-10 21:03:14 +0200

    fix #118

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -34,47 +34,5 @@ class IndexController extends Zend_Controller_Action {
         $videoMapper = new Application_Model_Mapper_Video();
         $this->view->video = $videoMapper->findLastVideo();
     }
-
-    public function contactAction() {
-        $this->view->headTitle()->append('Contact');
-        $this->view->headScript()->appendFile('/js/jquery/jquery.validate.min.js')
-                ->appendFile('/js/jquery/jquery.validate.localization/messages_fr.js')
-                ->appendFile('/js/contact.js');
-        $this->view->headLink()->appendStylesheet('/css/contact.css');
-        $contactMapper = new Application_Model_Mapper_Contact();
-        $contacts = $contactMapper->fetchAll();
-        $this->view->contact = $contacts[0];
-        $request = $this->getRequest();
-        $modifier = $request->getParam('modifier', false);
-        $this->view->modifier = $modifier;
-        $auth = Zend_Auth::getInstance();
-        if ($modifier && $auth->hasIdentity()
-                && $auth->getIdentity()->getRoleId() == Application_Model_Acl::ROLE_ADMIN) {
-
-            if (!is_null($firstName = $request->getParam('firstName')) && !is_null($email = $request->getParam('email'))) {
-                $contact = new Application_Model_Contact(array(
-                            'id' => 1,
-                            'prenom' => $firstName,
-                            'nom' => $request->getParam('lastName'),
-                            'telephone' => $request->getParam('phone'),
-                            'email' => $email,
-                        ));
-                $contactMapper->save($contact);
-                $this->_helper->flashMessenger('Modification du contact réussi');
-                $this->_redirect('/index/contact');
-            } else {
-                //TODO Message erreur
-            }
-        }
-    }
-
-    public function aproposAction() {
-        
-    }
-
-    public function mentionsAction() {
-        
-    }
-
 }
 
--- application/models/Acl.php
+++ application/models/Acl.php
@@ -40,6 +40,7 @@ class Application_Model_Acl extends Zend_Acl {
         // Dans notre cas, les ressources sont les controleurs et les privilèges 
         // sont les actions.
         $this->addResource('index')
+        		->addResource('autre')
                 ->addResource('calendrier')
                 ->addResource('club')
                 ->addResource('lieu')
@@ -55,6 +56,7 @@ class Application_Model_Acl extends Zend_Acl {
                 ->addResource('kym');
 
         $this->allow(self::ROLE_VISITEUR, array('index'), array('index', 'contact', 'apropos', 'mentions'))
+        		->allow(self::ROLE_VISITEUR, array('autre'), array('contact', 'apropos', 'mentions'))
                 ->allow(self::ROLE_VISITEUR, array('calendrier'), array('index', 'ical'))
                 ->allow(self::ROLE_VISITEUR, array('club'), array('index'))
                 ->allow(self::ROLE_VISITEUR, array('ultimate'), array('index'))
