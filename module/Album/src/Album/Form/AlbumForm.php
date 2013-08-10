e77b85b4b3a8c737bb957aed45eee30384d0f99c
EtienneM <titizebioutifoul@free.fr>
2013-08-10 15:57:51 +0200

    ZF2 Skeleton

---
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -0,0 +1,38 @@
+<?php
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+class AlbumForm extends Form {
+	public function __construct($name = null) {
+		parent::__construct('album');
+		$this->setAttribute('method', 'post');
+		$this->add(array(
+			'name' => 'id',
+			'type' => 'Hidden',
+		));
+		$this->add(array(
+			'name' => 'nom',
+			'type' => 'Text',
+			'options' => array(
+				'label' => 'Nom',
+			),
+		));
+		$this->add(array(
+			'name' => 'date',
+			'type' => 'Text',
+			'options' => array(
+				'label' => 'Date',
+			),
+		));
+		$this->add(array(
+			'name' => 'submit',
+			'type' => 'Submit',
+			'attributes' => array(
+				'value' => 'Go',
+				'id' => 'submitbutton'
+			),
+		));
+	}
+}
+
