b86c69b512d729d308c4bd0ff1d90607f09f0888
EtienneM <titizebioutifoul@free.fr>
2014-02-16 14:54:49 +0100

    Bugfix #123

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,4 +1,23 @@
 /**
+ * Equivalent to the php addslashes function 
+ * (http://fr2.php.net/manual/en/function.addslashes.php)
+ */
+function addslashes(str) {
+    // From: http://phpjs.org/functions
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Ates Goral (http://magnetiq.com)
+    // +   improved by: marrtins
+    // +   improved by: Nate
+    // +   improved by: Onno Marsman
+    // +   input by: Denny Wardhana
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Oskar Larsson Högfeldt (http://oskar-lh.name/)
+    // *     example 1: addslashes("kevin's birthday");
+    // *     returns 1: 'kevin\'s birthday'
+    return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+
+/**
  * Affiche un flash message.
  * Comme je ne suis pas certain de la librairie que je vais utiliser pour les
  * messages flashs, j'ai créé cette fonction et je ne modifierai qu'ici la 
@@ -10,7 +29,8 @@ function displayFlash(message) {
 
 var indexSlideshow = 0;
 function _slideshow(tabImgGalerie) {
-    $('div#bandeauDefilant').css('backgroundImage', 'url('+tabImgGalerie[indexSlideshow++]+')');
+    $('div#bandeauDefilant').css('backgroundImage', 'url('+addslashes(tabImgGalerie[indexSlideshow])+')');
+    indexSlideshow++;
     if (indexSlideshow >= tabImgGalerie.length) {
         indexSlideshow = 0;
     }
