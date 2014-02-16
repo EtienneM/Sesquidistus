329bd363f09d26e77e0f798e173750e2731e1801
EtienneM <titizebioutifoul@free.fr>
2014-02-16 18:26:58 +0100

    Bugfix regexp

--- application/controllers/helpers/My/Helper/Slideshow/GooglePlus.php
+++ application/controllers/helpers/My/Helper/Slideshow/GooglePlus.php
@@ -2,7 +2,7 @@
 
 class My_Helper_Slideshow_GooglePlus implements My_Helper_Slideshow_Provider {
 	public function getEmbeddedCode($url, $height = null, $width = null) {
-		if(!preg_match('/^https?:\/\/.+\/photos\/(.+)\/albums\/(.+)/', $url, $matches)) {
+		if(!preg_match('/^https?:\/\/.+\/photos\/([0-9]+)\/albums\/([0-9]+)/', $url, $matches)) {
 			throw new Exception("Impossible to determine the user ID and the album ID");
 		}
 		return My_Helper_Slideshow_Google::getEmbeddedCode($matches[1], $matches[2], $height, $width);
